59c3a6ddb8ab25b8542c431380e933bdc347bd1c
mahoya <support@maho-ya.com>
2021-03-13 11:09:36 +0900

    Add icons

---
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/AppIcons.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.androiddevchallenge.ui.theme
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.FilterList
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.ShoppingCart
+
+class AppIcons {
+    companion object {
+        val accountCircle = Icons.Filled.AccountCircle
+        val search = Icons.Filled.Search
+        val favoriteBorder = Icons.Filled.FavoriteBorder
+        val home = Icons.Filled.Home
+        val shoppingCart = Icons.Filled.ShoppingCart
+        val filterList = Icons.Filled.FilterList
+        val done = Icons.Filled.Done
+    }
+}
