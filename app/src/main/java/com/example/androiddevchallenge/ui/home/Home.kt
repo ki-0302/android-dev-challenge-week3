ceb45f133ed2d9b81520f09e2d4a51a09b8d38cb
mahoya <support@maho-ya.com>
2021-03-13 11:32:51 +0900

    Add screens

---
+++ app/src/main/java/com/example/androiddevchallenge/ui/home/Home.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.androiddevchallenge.ui.home
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.androiddevchallenge.ui.theme.MyTheme
+
+@Composable
+fun Home(
+    modifier: Modifier = Modifier,
+    navController: NavController,
+    darkTheme: Boolean = isSystemInDarkTheme()
+) {
+    MyTheme(darkTheme = darkTheme) {
+        Scaffold { innerPadding ->
+            modifier.padding(innerPadding)
+        }
+    }
+}
+
+@Preview("Light Theme", widthDp = 360, heightDp = 640)
+@Composable
+fun HomeLightPreview() {
+    HomePreview(darkTheme = false)
+}
+
+@Preview("Dark Theme", widthDp = 360, heightDp = 640)
+@Composable
+fun HomeDarkPreview() {
+    HomePreview(darkTheme = true)
+}
+
+@Composable
+fun HomePreview(darkTheme: Boolean = false) {
+    Home(
+        navController = rememberNavController(),
+        darkTheme = darkTheme
+    )
+}
